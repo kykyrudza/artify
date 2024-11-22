a9ab672462b04b583d47b95556e287188cf9bf3e
Smakouz Illea <illeasmakouz@gmail.com>
2024-11-22 20:06:30 +0200

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'] )->name('home')->middleware('guest');
+
+Route::get('/dashboard', [HomeController::class, 'dashboard'] )->name('dashboard')->middleware('auth');
+
+Route::get('/login', [HomeController::class, 'login'] )->name('login')->middleware('guest');
+
+Route::get('/register', [HomeController::class, 'register'] )->name('register')->middleware('guest');
