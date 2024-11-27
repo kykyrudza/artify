13bd5ef54d70dd7e8165b8c6129b12a5e435ed47
Smakouz Illea <illeasmakouz@gmail.com>
2024-11-27 22:24:54 +0200

    edits and improvements

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Actions\CreateTaskAction;
 use App\Http\Requests\CreateTaskRequest;
-use App\Models\Task;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
